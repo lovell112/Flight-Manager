a8ce403747b88ff71fe733c659326edd9b163ab7
lovell112 <xuansontrannguyen06@gmail.com>
2025-11-07 13:44:48 +0700

    config cmakelists

--- include/repository/AdminRepository.h
+++ include/repository/AdminRepository.h
@@ -1,12 +1,17 @@
-//
-// Created by HP on 05/11/2025.
-//
-
 #ifndef FLIGHTMANAGER_ADMINREPOSITORY_H
 #define FLIGHTMANAGER_ADMINREPOSITORY_H
-
+#include <string>
+#include <vector>
+using namespace std;
 
 class AdminRepository {
+    vector<AdminRepository*> m_adminRepository;
+    string PATH = "../../data/admin.txt";
+public:
+    void loadAllAdmins() const;
+    void saveAllAdmins() const;
+    bool validateLogin(const string&, const string&) const;
+    bool changePassword(const string&, const string&);
 };
 
 
--- src/repository/AdminRepository.cpp
+++ src/repository/AdminRepository.cpp
@@ -1,5 +1,1 @@
-//
-// Created by HP on 05/11/2025.
-//
-
 #include "../../include/repository/AdminRepository.h"
