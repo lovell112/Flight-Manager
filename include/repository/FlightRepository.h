ecbd52724313fa663669abbc1c25a894f76356c2
minh <5001104091@student.hcmue.edu.vn>
2025-11-08 19:04:40 +0700

    Update FlightRepository.h

--- include/repository/FlightRepository.h
+++ include/repository/FlightRepository.h
@@ -4,9 +4,27 @@
 
 #ifndef FLIGHTMANAGER_FLÌGHTREPOSITORY_H
 #define FLIGHTMANAGER_FLÌGHTREPOSITORY_H
-
+#include "../core/Ticket.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
 class FlightRepository {
+    vector<Ticket*> m_tickets;
+    const string PATH = "../data/ticket.txt";
+public:
+    TicketRepository();
+    ~TicketRepository();
+    void add(const Ticket& ticket);
+    void remove(const string& ticketID);
+    vector<Ticket*>::iterator findByID(const string& id);
+    vector<Ticket*> findByDate(const string& date);
+    vector<Ticket*> findByDestination(const string& destination);
+    vector<Ticket*>& getAll();
+    void loadAllTickets();
+    void saveAllTickets();
 };
 
 
