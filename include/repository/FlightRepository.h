29fad4769036e6f40b06ed1194ead4d9707c9d5d
belongkawaii <binlun291006@gmail.com>
2025-11-08 20:59:59 +0700

    fligt

--- include/repository/FlightRepository.h
+++ include/repository/FlightRepository.h
@@ -2,12 +2,33 @@
 // Created by HP on 05/11/2025.
 //
 
-#ifndef FLIGHTMANAGER_FLÌGHTREPOSITORY_H
-#define FLIGHTMANAGER_FLÌGHTREPOSITORY_H
+#ifndef FLIGHTMANAGER_FLIGHTREPOSITORY_H
+#define FLIGHTMANAGER_FLIGHTREPOSITORY_H
 
+#include "../core/Admin.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
 class FlightRepository {
-};
+private:
+    const string PATH = "../data/flight.txt";
+    vector<Flight> m_flights;
 
+public:
+    void add(const Flight&);
+    void remove(const string&);
+    void setFlightStatus(const string&, const FlightStatus&);
+
+    vector<Flight>::iterator findByID(const string&);
+    vector<Flight*> findByDate(const string&);
+    vector<Flight*> findByDestination(const string&);
+
+    vector<Flight*>& getAll();
+    void loadAllFlights();
+    void saveAllFlights();
+};
 
 #endif //FLIGHTMANAGER_FLÌGHTREPOSITORY_H
--- src/repository/FlightRepository.cpp
+++ src/repository/FlightRepository.cpp
@@ -3,3 +3,136 @@
 //
 
 #include "../../include/repository/FlightRepository.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+// ------------------------------------------------------
+// them chuyen bay moi
+// ------------------------------------------------------
+void FlightRepository::add(const Flight& flight) {
+	m_flights.push_back(flight); // them vao vector
+}
+
+// ------------------------------------------------------
+// xoa chuyen bay
+// ------------------------------------------------------
+void FlightRepository::remove(const string& flightID) {
+	for (auto it = m_flights.begin(); it != m_flights.end(); ++it) { // duyet vector
+		if (it->getFlightID() == flightID) { // neu tim thay chuyen bay can xoa
+            m_flights.erase(it);
+            break;
+        }
+    }
+}
+
+// ------------------------------------------------------
+// cap nhat trang thai chuyen bay
+// ------------------------------------------------------
+void FlightRepository::setFlightStatus(const string& flightID, const FlightStatus& status) {
+	auto it = findByID(flightID); // dung ham tim kiem
+	if (it != m_flights.end()) {  // cap nhat neu tim thay
+        it->setStatus(status);
+    }
+}
+
+// ------------------------------------------------------
+// tim theo ID
+// ------------------------------------------------------
+vector<Flight>::iterator FlightRepository::findByID(const string& flightID) {
+    for (auto it = m_flights.begin(); it != m_flights.end(); ++it) {
+        if (it->getFlightID() == flightID)
+			return it; // tra ve iterator neu tim thay
+    }
+	return m_flights.end(); // khong tim thay tra ve end()
+}
+
+// ------------------------------------------------------
+// tim theo ngay
+// ------------------------------------------------------
+vector<Flight*> FlightRepository::findByDate(const string& dateString) {
+    vector<Flight*> result;
+    for (auto& f : m_flights) {
+        if (f.getDepartureDate().toString() == dateString) { // so sanh
+			result.push_back(&f); // them vao ket qua
+        }
+    }
+    return result;
+}
+
+// ------------------------------------------------------
+// tim theo destination
+// ------------------------------------------------------
+vector<Flight*> FlightRepository::findByDestination(const string& destination) {
+    vector<Flight*> result;
+    for (auto& f : m_flights) {
+        if (f.getDestinationAirport() == destination) {
+            result.push_back(&f);
+        }
+    }
+    return result;
+}
+
+// ------------------------------------------------------
+// lay tat ca chuyen bay
+// ------------------------------------------------------
+vector<Flight*>& FlightRepository::getAll() {
+    static vector<Flight*> all;
+    all.clear();
+    for (auto& f : m_flights)
+        all.push_back(&f);
+    return all;
+}
+
+// ------------------------------------------------------
+// doc toan bo du lieu chuyen bay tu file flights.txt
+// ------------------------------------------------------
+void FlightRepository::loadAllFlights() {
+    ifstream fin(PATH);
+    if (!fin.is_open()) {
+        cerr << "Khong doc duoc file" << endl;
+        return;
+    }
+
+    m_flights.clear();
+    string line;
+    while (getline(fin, line)) {
+        stringstream ss(line);
+        string id, plane, dest;
+        int day, month, year, hour, minute, statusInt;
+
+        ss >> id >> plane >> dest >> day >> month >> year >> hour >> minute >> statusInt;
+
+        DateTime dt{ day, month, year, hour, minute };
+        FlightStatus status = static_cast<FlightStatus>(statusInt);
+        Flight f(id, plane, dest, dt, status);
+        m_flights.push_back(f);
+    }
+
+    fin.close();
+}
+
+// ------------------------------------------------------
+// ghi toan bo du lieu chuyen bay vao file flights.txt
+// ------------------------------------------------------
+void FlightRepository::saveAllFlights() {
+    ofstream fout(PATH);
+    if (!fout.is_open()) {
+        cerr << "Khong viet duoc file" << endl;
+        return;
+    }
+
+    for (auto& f : m_flights) {
+        DateTime d = f.getDepartureDate();
+        fout << f.getFlightID() << " "
+            << f.getAirplaneID() << " "
+            << f.getDestinationAirport() << " "
+            << d.day << " " << d.month << " " << d.year << " "
+            << d.hour << " " << d.minute << " "
+            << static_cast<int>(f.getStatus()) << "\n";
+    }
+
+    fout.close();
+}
