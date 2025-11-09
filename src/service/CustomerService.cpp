3edb4e7c6aa83691e541b8fba197c2d8b9978f61
Ngô Huỳnh Anh Quốc <anhquocanxien16a6@gmail.com>
2025-11-09 14:24:59 +0700

    Add CustomerService class with basic CRUD operations
    Implemented CustomerService class with methods to add, remove, and find customers.

--- src/service/CustomerService.cpp
+++ src/service/CustomerService.cpp
@@ -1,5 +1,36 @@
-//
-// Created by HP on 05/11/2025.
-//
-
 #include "../../include/service/CustomerService.h"
+#include <iostream>
+
+// Constructor - khoi tao repository
+CustomerService::CustomerService() {
+    m_customerRepository = new CustomerRepository();
+}
+
+CustomerService::~CustomerService() {
+    cout << "Delete CustomerService thanh cong\n";
+    delete m_customerRepository;
+}
+
+// them khach hang vao repository
+void CustomerService::addCustomer(const Customer& customer) {
+    m_customerRepository->add(customer);
+}
+
+// - xoa khach hang theo ID
+void CustomerService::removeCustomer(const string& customerID) {
+    m_customerRepository->remove(customerID);
+}
+
+//tim khach hang theo ID, tra ve iterator
+vector<Customer*>::iterator CustomerService::findCustomerByID(const string& customerID) {
+    return m_customerRepository->findByID(customerID);
+}
+
+//lay tat ca khach hang, tra ve iterator dau tien
+vector<Customer*>::iterator CustomerService::getAll() {
+    return m_customerRepository->getAll();
+}
+// end - lay iterator cuoi cung
+vector<Customer*>::iterator CustomerService::end() {
+    return m_customerRepository->end();
+}
