dd3ee51ae69b2add682c0e9b9583f5ff5767573b
minh <5001104091@student.hcmue.edu.vn>
2025-11-21 10:29:40 +0700

    Update List.hpp

--- include/datastructures/List.hpp
+++ include/datastructures/List.hpp
@@ -1,7 +1,8 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+
 #include <cstddef>
-using namespace std;
+#include <stdexcept>
 
 template <typename T>
 class List {
@@ -9,36 +10,140 @@ class List {
     size_t m_iSize;
     size_t m_iCapacity;
 
-    void copy();
+    // Tăng dung lượng khi cần
+    void reserve(size_t newCapacity) {
+        if (newCapacity <= m_iCapacity) return;
+
+        T* newArr = new T[newCapacity];
+        for (size_t i = 0; i < m_iSize; i++)
+            newArr[i] = m_pArray[i];
+
+        delete[] m_pArray;
+        m_pArray = newArr;
+        m_iCapacity = newCapacity;
+    }
+
 public:
-    List();
-    ~List();
-    void add(T item);
-    void remove(size_t index);
-    bool contains(T key) const;
-    void insert(size_t index, const T& value);
-    size_t size() const;
-    size_t capacity() const;
-    void clear();
-    void reserve(size_t newCapacity);
-    void resize(size_t newSize);
-    T& operator[](size_t) const;
+    // --- Constructor ---
+    List() : m_pArray(nullptr), m_iSize(0), m_iCapacity(0) {}
+
+    // --- Destructor ---
+    ~List() {
+        delete[] m_pArray;
+    }
+
+    // --- Add item ---
+    void add(T item) {
+        if (m_iSize == m_iCapacity)
+            reserve(m_iCapacity == 0 ? 2 : m_iCapacity * 2);
+        m_pArray[m_iSize++] = item;
+    }
+
+    // --- Remove item ---
+    void remove(size_t index) {
+        if (index >= m_iSize) throw std::out_of_range("Index out of range");
+        for (size_t i = index; i < m_iSize - 1; i++)
+            m_pArray[i] = m_pArray[i + 1];
+        m_iSize--;
+    }
+
+    // --- Check contains ---
+    bool contains(T key) const {
+        for (size_t i = 0; i < m_iSize; i++)
+            if (m_pArray[i] == key)
+                return true;
+        return false;
+    }
+
+    // --- Insert at index ---
+    void insert(size_t index, const T& value) {
+        if (index > m_iSize) throw std::out_of_range("Index out of range");
+
+        if (m_iSize == m_iCapacity)
+            reserve(m_iCapacity == 0 ? 2 : m_iCapacity * 2);
+
+        for (size_t i = m_iSize; i > index; i--)
+            m_pArray[i] = m_pArray[i - 1];
+
+        m_pArray[index] = value;
+        m_iSize++;
+    }
+
+    // --- Get size ---
+    size_t size() const { return m_iSize; }
+
+    // --- Get capacity ---
+    size_t capacity() const { return m_iCapacity; }
+
+    // --- Clear ---
+    void clear() { m_iSize = 0; }
+
+    // --- Resize ---
+    void resize(size_t newSize) {
+        if (newSize > m_iCapacity)
+            reserve(newSize * 2);
+        m_iSize = newSize;
+    }
+
+    // --- Operator [] ---
+    T& operator[](size_t index) const {
+        if (index >= m_iSize) throw std::out_of_range("Index out of range");
+        return m_pArray[index];
+    }
+
+    // =======================
+    //      ITERATOR
+    // =======================
     class iterator {
         T* m_pointer;
     public:
-        iterator();
-        ~iterator();
-        T& operator*() const;
-        iterator& operator++();
-        iterator operator++(int);
-        iterator& operator--();
-        iterator operator--(int);
-        bool operator==(const iterator&) const;
-        bool operator!=(const iterator&) const;
-        bool empty() const;
+        iterator(T* ptr = nullptr) : m_pointer(ptr) {}
+        ~iterator() {}
+
+        T& operator*() const { return *m_pointer; }
+
+        iterator& operator++() {  // prefix ++i
+            m_pointer++;
+            return *this;
+        }
+
+        iterator operator++(int) {  // postfix i++
+            iterator temp = *this;
+            m_pointer++;
+            return temp;
+        }
+
+        iterator& operator--() {
+            m_pointer--;
+            return *this;
+        }
+
+        iterator operator--(int) {
+            iterator temp = *this;
+            m_pointer--;
+            return temp;
+        }
+
+        bool operator==(const iterator& other) const {
+            return m_pointer == other.m_pointer;
+        }
+
+        bool operator!=(const iterator& other) const {
+            return m_pointer != other.m_pointer;
+        }
+
+        bool empty() const {
+            return m_pointer == nullptr;
+        }
     };
-    iterator begin();
-    iterator end();
+
+    iterator begin() {
+        return iterator(m_pArray);
+    }
+
+    iterator end() {
+        return iterator(m_pArray + m_iSize);
+    }
 };
 
 #endif
